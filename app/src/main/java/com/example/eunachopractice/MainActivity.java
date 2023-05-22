8bbb52c1a46eecde71683fb2e629fc79aaab1800
= <=>
2023-05-22 14:34:32 +0900

    message

--- app/src/main/java/com/example/eunachopractice/MainActivity.java
+++ app/src/main/java/com/example/eunachopractice/MainActivity.java
@@ -4,9 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean isfirstInput = true; // 입력값이 처음 입력되는가를 체크
+    int resultNumber = 0; // 결과 변수
+    char operator = '+'; // 입력 변수
+
+    TextView resultText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
