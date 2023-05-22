4c33b2f7d42459c928902d81bed8369a320764b1
= <=>
2023-05-22 17:36:41 +0900

    commit

--- app/src/main/java/com/example/eunachopractice/MainActivity.java
+++ app/src/main/java/com/example/eunachopractice/MainActivity.java
@@ -3,13 +3,15 @@ package com.example.eunachopractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     int resultNum = 0;
-
+    boolean isfirstInput = true;
+    char operator = '+';
     TextView resultText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,16 +23,13 @@ public class MainActivity extends AppCompatActivity {
     public void buttonClick(View view) {
         Button getButton = findViewById(view.getId());
         switch(view.getId()){
-            
+            default:
+                //   Toast.makeText(getApplicationContext(),getButton.getText().toString()
+                //         + "버튼이 클릭되었습니다", Toast.LENGTH_LONG).show();
+                Log.e("buttonClick","default" + getButton.getText().toString()
+                        + "버튼이 클릭되었습니다");
+                break;
         }
-        /*
-        if(view.getId() == R.id.all_clear_button){
-            isfirstInput = true;
-            resultNumber = 0;
-            operator = '+';
-            resultText.setTextColor(0xFF666666);
-            resultText.setText(String.valueOf(resultNumber));
-        }
-*/
+
     }
 }
